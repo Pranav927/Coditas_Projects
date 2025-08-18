c0bb50d1c7663be7204ba9b204bfd617e019fee7
Pranav Phadke <146724939+Pranav927@users.noreply.github.com>
2025-08-18 16:55:30 +0530

    Add files via upload

---
+++ Inventory_Management/mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="Aditi@279",
+)
+
+#prepare cursor object
+cursorObject = dataBase.cursor()
+
+#create database
+cursorObject.execute("CREATE DATABASE inventory")
+
+print("All done")
